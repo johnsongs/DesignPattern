48caa432c95a029d88ee7a0c31abfcad6c8f4b6a
Johnson <johnson_gs@live.cn>
2018-08-24 18:12:09 +0800

    抽象工厂 init

---
+++ DPAbstractFactory/DPAbstractFactory/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  DPAbstractFactory
+//
+//  Created by Johnson on 2018/8/24.
+//  Copyright © 2018年 Johnson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
