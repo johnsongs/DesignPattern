792b28d543804b0515221774933e423c887a45a4
Johnson <johnson_gs@live.cn>
2018-08-27 09:13:30 +0800

    update

---
+++ DPAbstractFactory/DPAbstractFactory/DPAMetaFactory.h
@@ -0,0 +1,13 @@
+//
+//  DPAMetaFactory.h
+//  DPAbstractFactory
+//
+//  Created by Johnson on 2018/8/25.
+//  Copyright © 2018年 Johnson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DPAMetaFactory : NSObject
+
+@end
---
+++ DPAbstractFactory/DPAbstractFactory/DPAMetaFactory.m
@@ -0,0 +1,13 @@
+//
+//  DPAMetaFactory.m
+//  DPAbstractFactory
+//
+//  Created by Johnson on 2018/8/25.
+//  Copyright © 2018年 Johnson. All rights reserved.
+//
+
+#import "DPAMetaFactory.h"
+
+@implementation DPAMetaFactory
+
+@end
